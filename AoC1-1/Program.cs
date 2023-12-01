95cd8846df3d05ac2290bd7275fa2ed659be87b8
Samuele Lorefice <samuelelorefice98@gmail.com>
2023-12-01 21:27:09 +0100

    Added solutions for first day

---
+++ AoC1-1/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] input = File.ReadAllLines("input1.txt");
+int[] numbers = new int[input.Length];
+Func<string, char?> FindFirstNumeral = s => s.FirstOrDefault(c => char.IsDigit(c));
+for (int i = 0; i < input.Length; i++) {
+    var digits = new char[2];
+    digits[0] = FindFirstNumeral(input[i]).Value;
+    digits[1] = FindFirstNumeral(new String(input[i].Reverse().ToArray())).Value;
+    numbers[i] = Int32.Parse(digits);
+}
+Console.WriteLine(numbers.Sum());
+return numbers.Sum();
---
+++ AoC1-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using AoC1_2;
+using System.Text.RegularExpressions;
+
+string[] input = File.ReadAllLines("input1.txt");
+int[] numbers = new int[input.Length];
+Func<string, int?> FindNumber = s => {
+    var pattern = String.Join("|", Enum.GetNames(typeof(DigitWord)));
+    var match = Regex.Match(s, @"^(" + pattern + @")", RegexOptions.IgnoreCase);
+    return match.Success ? (int)Enum.Parse<DigitWord>(match.Value, true) : null;
+};
+Func<string, List<int>> NumberifyString= s => {
+    var numbers = new List<int>();
+    for (int i = 0; i < s.Length; i++) {
+        if(Char.IsDigit(s[i])){
+            numbers.Add(Int32.Parse(s[i].ToString()));
+            continue;
+        }
+        if (FindNumber(s.Substring(i)) != null) {
+            numbers.Add((int)FindNumber(s.Substring(i)));
+        }
+    }
+    return numbers;
+};
+
+for (int i = 0; i < input.Length; i++) {
+    var numbersInString = NumberifyString(input[i]);
+    numbers[i] = numbersInString.First()*10+ numbersInString.Last();
+}
+return numbers.Sum();
