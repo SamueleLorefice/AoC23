ce8014c6cdbfe4e4230630192cbd57e03d4a8244
Samuele Lorefice <samuelelorefice98@gmail.com>
2023-12-02 18:44:12 +0100

    Added solutions for second day

---
+++ AoC2-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+string[] input = File.ReadAllLines("input.txt");
+bool[] possible = new bool[input.Length+1];
+possible[0] = false;
+var maxRed = 12;
+var maxGreen = 13;
+var maxBlue = 14;
+Regex gameParse = new Regex("Game (?<id>[0-9]+): (?:(?:(?<count>[0-9]+) (?<color>red|green|blue))(?:(?<pass>,)|(?<end>;))* *)+", RegexOptions.Compiled|RegexOptions.IgnoreCase);
+Parallel.ForEach(input, (line, _, index) => {
+    var match = gameParse.Match(line);
+    var id = int.Parse(match.Groups["id"].Value);
+    var counts = match.Groups["count"].Captures;
+    var colors = match.Groups["color"].Captures;
+    var ends = match.Groups["end"].Captures;
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+    Action reset = () => {
+        red = 0;
+        green = 0;
+        blue = 0;
+    };
+    for (int i = 0; i < counts.Count; i++) {
+        if (ends.Any(capture => capture.Index == counts[i].Index - 2)) {
+            possible[id] = red <= maxRed && green <= maxGreen && blue <= maxBlue;
+            if (!possible[id]) break;
+            reset();
+        }
+        switch (colors[i].Value) {
+            case "red":
+                red += int.Parse(counts[i].Value);
+                break;
+            case "green":
+                green += int.Parse(counts[i].Value);
+                break;
+            case "blue":
+                blue += int.Parse(counts[i].Value);
+                break;
+        }
+    }
+    if (possible[id]) {
+        possible[id] = red <= maxRed && green <= maxGreen && blue <= maxBlue;
+    }
+});
+return possible.Select((value, i) => new { value, index = i }).Where(x => x.value).Sum(x => x.index);
---
+++ AoC2-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+string[] input = File.ReadAllLines("input.txt");
+int[] powers = new int[input.Length+1];
+
+Regex gameParse = new Regex("Game (?<id>[0-9]+): (?:(?:(?<count>[0-9]+) (?<color>red|green|blue))(?:(?<pass>,)|(?<end>;))* *)+", RegexOptions.Compiled|RegexOptions.IgnoreCase);
+Parallel.ForEach(input, (line, _, index) => {
+    var match = gameParse.Match(line);
+    var id = int.Parse(match.Groups["id"].Value);
+    var counts = match.Groups["count"].Captures;
+    var colors = match.Groups["color"].Captures;
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+    for (int i = 0; i < counts.Count; i++) {
+        var newNum = int.Parse(counts[i].Value);
+        switch (colors[i].Value) {
+            case "red":
+                red = newNum > red ? newNum : red;  
+                break;
+            case "green":
+                green = newNum > green ? newNum : green;
+                break;
+            case "blue":
+                blue = newNum > blue ? newNum : blue;
+                break;
+        }
+    }
+    powers[id] = red * green * blue;
+});
+return powers.Sum();
